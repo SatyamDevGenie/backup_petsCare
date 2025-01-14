53f604a23047f9e8ebafb70ecde0c0ca36427cae
SatyamDevGenie <satyamsawant54@gmail.com>
2025-01-14 21:43:50 +0530

    Cloned - petsCare Original Repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white font-extrabold text-4xl">
+      petsCare Application 2025
+    </div>
+  );
+}
+
+export default App;
